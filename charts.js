797db5c747715e18e51f65c8abf1d74adc69c2a7
jfharris67 <114355199+jfharris67@users.noreply.github.com>
2023-02-28 04:05:31 -0500

    update chart.js bugs

--- charts.js
+++ charts.js
@@ -36,7 +36,7 @@ function buildMetadata(sample) {
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
-   
+    
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
 
@@ -114,40 +114,7 @@ Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
 // Deliverable 3: 4. Create the trace for the gauge chart.
 var gaugeData = [  {    domain: { x: [0, 1], y: [0, 1] },
-// Deliverable 1: 7. Create the yticks for the bar chart.
-// Hint: Get the the top 10 otu_ids and map them in descending order 
-// so the otu_ids with the most bacteria are last. 
-var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
-
-// Deliverable 1: 8. Create the trace for the bar chart. 
-var barData = [  {    x: sample_values.slice(0, 10).reverse(),    y: yticks,    text: otu_labels.slice(0, 10).reverse(),    type: "bar",    orientation: "h",  }];
-
-// Deliverable 1: 9. Create the layout for the bar chart. 
-var barLayout = {
-  title: "Top 10 Bacteria Cultures Found",
-  margin: {t: 30, l: 150}
-};
-
-// Deliverable 1: 10. Use Plotly to plot the data with the layout. 
-Plotly.newPlot("bar", barData, barLayout);
-
-// Deliverable 2: 1. Create the trace for the bubble chart.
-var bubbleData = [  {    x: otu_ids,    y: sample_values,    text: otu_labels,    mode: "markers",    marker: {      size: sample_values,      color: otu_ids,      colorscale: "Earth"    }  }];
-
-// Deliverable 2: 2. Create the layout for the bubble chart.
-var bubbleLayout = {
-  title: "Bacteria Cultures Per Sample",
-  xaxis: {title: "OTU ID"},
-  automargin: true,
-  hovermode: "closest"
-};
-
-// Deliverable 2: 3. Use Plotly to plot the data with the layout.
-Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-
-// Deliverable 3: 4. Create the trace for the gauge chart.
-var gaugeData = [  {    domain: { x: [0, 1], y: [0, 1] },
-    value: wfreq,
+    value: washingFrequency,
     title: { text: "Scrubs per Week" },
     type: "indicator",
     mode: "gauge+number",
